db5375f7221873db099cc2b09a5bd125462b5b1a
Vineel203 <vineel.vineel203@gmail.com>
2019-04-17 00:25:49 +0530

    intial Commit

--- training/AutoEncoder.py
+++ training/AutoEncoder.py
@@ -5,6 +5,8 @@ from keras.datasets import mnist
 import numpy as np
 import preprocess
 
+
+
 class AutoEncoder(object):
     def __init__(self,inputSize,hiddenNumber):
         self.encoding_dim = hiddenNumber
@@ -21,7 +23,7 @@ class AutoEncoder(object):
         self.decoder_layer = self.autoencoder.layers[-1]
         self.decoder = Model(encoded_input, self.decoder_layer(encoded_input))
         self.autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-        plot_model(self.autoencoder, to_file='model.png')
+
 
     def train(self,x_train,y_train,epochs):
         self.autoencoder.fit(x_train, y_train,
@@ -29,6 +31,7 @@ class AutoEncoder(object):
                         batch_size=256,
                         shuffle=True,
                         validation_data=(x_test, x_test))
+        
 
     def predict(self,x_test):
         return self.autoencoder.predict(x_test)
@@ -49,8 +52,8 @@ class StackedAutoEncoder(object):
         plot_model(self.sae, to_file='model.png')
 
     def train(self,x_train,y_train,epochs,percentagesList):
-        pass
-
+    	
+        
 
     def predict(self,x_test):
         return self.sae.predict(x_test)
@@ -61,3 +64,36 @@ class StackedAutoEncoder(object):
     def load(self,name):
         self.sae.load("../Model/"+name+".h5")
 
+
+from keras.datasets import mnist
+import numpy as np
+
+(x_train, _), (x_test, _) = mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+ae = AutoEncoder(784,4000)
+ae.train(x_train,x_train,10)
+
+encoded_imgs = ae.predict(x_test)
+
+import matplotlib.pyplot as plt
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(encoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
